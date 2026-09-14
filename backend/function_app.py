a0087f2b2b610c18cc559a5fc0e5d8cde3444ba4
Rafael Lule <rafael.lule10@yahoo.com>
2026-09-14 11:25:09 -0500

    fix: Updated html to include certifications, and refactored functions.

--- backend/function_app.py
+++ backend/function_app.py
@@ -7,44 +7,49 @@ from azure.cosmos import CosmosClient, exceptions
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+try:
+    # Database and container names
+    database_name = "db-crc-dev-ctus"
+    container_name = "visitors"
+
+    # Create cosmos client and container
+    db_url: str | None = os.environ.get("COSMOS_DB_URL")
+    db_key: str | None = os.environ.get("COSMOS_DB_KEY")
+
+    if not db_url or not db_key:
+        raise ValueError("COSMOS_DB_URL and COSMOS_DB_KEY must be set.")
+
+    client = CosmosClient(db_url, credential=db_key)
+
+    # Get the database and container
+    container = client.get_database_client(database_name).get_container_client(container_name)
+
+except Exception as e:
+    logging.error(f"Failed to initialize Cosmos DB client: {e}")
+    raise
 
 @app.route(route="GetVisitorCount")
 def GetVisitorCount(req: func.HttpRequest) -> func.HttpResponse:
     try:
         logging.info("Python HTTP trigger function processed a request.")
 
-        db_url: str | None = os.environ.get("COSMOS_DB_URL")
-
-        db_key: str | None = os.environ.get("COSMOS_DB_KEY")
-
-        if not db_url or not db_key:
-            raise ValueError("COSMOS_DB_URL and COSMOS_DB_KEY must be set.")
-
-        # Create an instance of the CosmosClient using the URL and key
-        client = CosmosClient(db_url, credential=db_key)
-
-        # Get the database
-        database_name = client.get_database_client("db-crc-dev-ctus")
-
-        # Get the container
-        container_name = database_name.get_container_client("visitors")
-
-        item = container_name.read_item(item="1", partition_key="1")
+        item = container.read_item(item="1", partition_key="1")
 
         item["visitors"] = item.get("visitors", 0) + 1
 
-        container_name.replace_item(item=item["id"], body=item)
+        container.replace_item(item=item["id"], body=item)
 
-        print(item)
         return func.HttpResponse(
             body=json.dumps({"visitors": item["visitors"]}),
             status_code=200,
             mimetype="application/json",
         )
+    
     except exceptions.CosmosResourceNotFoundError:
         item = {"id": "1", "visitors": 1}
-        container_name.create_item(body=item)  # type: ignore
+        container.create_item(body=item)  # type: ignore
         return func.HttpResponse(f"Visitor count: {item}", status_code=200)
+    
     except Exception as e:
         logging.error(f"An error occurred: {e}")
         return func.HttpResponse(
