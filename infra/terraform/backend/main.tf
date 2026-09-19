fd72e6caf04ddcc142e20ab3ca73f0950ffd0bc5
Rafael Lule <rafael.lule10@yahoo.com>
2026-09-18 22:29:58 -0500

    fix: Updated backend terraform Changed the public_network_access_enabled to public_network_access to fix deprication warning.

--- infra/terraform/backend/main.tf
+++ infra/terraform/backend/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_storage_account" "resume" {
   account_tier                  = "Standard"
   account_replication_type      = "LRS"
   account_kind                  = "StorageV2"
-  public_network_access_enabled = true
+  public_network_access = "Enabled"
 
   tags = {
     environment = local.env
